76a97c587da70e59e003510bb080d5d8cae1363b
AigulD <70228729+AigulD@users.noreply.github.com>
2020-09-28 10:10:44 +0500

    corrected

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        BmiService service = new BmiService();
         double weightkg = 55;
         double heightm = 1.62;
+
+        BmiService service = new BmiService();
+
         double bmi = service.calculate(weightkg,heightm);
         System.out.printf("Body Mass Index %.2f",bmi);
     }
